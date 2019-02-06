e99877e96aa5d227fcffb383fd10d22c6702018d
Abhinav Goyal <agabohar@gmail.com>
2019-02-06 14:57:27 +0530

    A2: P2 completed (need verification)

--- A2/Q2/code/homography.m
+++ A2/Q2/code/homography.m
@@ -1,7 +1,8 @@
 function [ H ] = homography( p1, p2 )
-%HOMOGRAPHY Summary of this function goes here
-%   Detailed explanation goes here
-
-
+    [n,d] = size(p1);
+    X1 = [p1 ones(n,1)]; % Points in homogenous co-ordinate system
+    M = [-X1 zeros(n,d+1) p2(:,1).*X1; zeros(n,d+1) -X1 p2(:,2).*X1]; % M matrix in Mx = 0
+    [~,~,V] = svd(M); % SVD to find argmin_x Mx
+    H = reshape(V(:,end),3,3)'; % 2D homography matrix
 end
 
--- A2/Q2/code/myMainScript.m
+++ A2/Q2/code/myMainScript.m
@@ -1,6 +1,33 @@
-%% Rigit Transform between 2 sets of 3D Points
+%% Load Data and visualize data
+load('../input/Q2data.mat');
 
-%% Load Data
-load('../input/Q1data.mat');
+X1 = box_coords;
 
-%% Your code here
+% Marked points
+marked = insertMarker(im, X1, 'Color', 'red', 'Size', 10);
+marked = insertMarker(marked, area_coords, 'Color', 'black', 'Size', 10);
+imshow(marked);
+title("Selected points for estimating homography and field dimensions");
+
+% Corresponding co-ordinates for top view image of the outer Dee (box)
+scaling_factor = 15;
+width = scaling_factor*width;
+height = scaling_factor*height;
+X2 = [0 0; width 0; 0 height; width height];
+
+im_h = zeros(height, width);
+figure;
+imshow(im_h);
+title("Scaled top view skeleton image of the outer box");
+
+%% Estimate Homography matrix and find playing area dimensions
+H = homography(X1, X2);
+n = size(area_coords,1);
+X = [area_coords'; ones(1,n)];
+X_h = H*X;
+pts = X_h(1:2,:)./X_h(3,:);
+
+area_width = round(sqrt(sum((pts(:,2) - pts(:,1)).^2))/scaling_factor);
+area_height = round(sqrt(sum((pts(:,3) - pts(:,2)).^2))/scaling_factor);
+
+fprintf(1, "Playing area dimensions: %dyd x %dyd", area_width, area_height);
