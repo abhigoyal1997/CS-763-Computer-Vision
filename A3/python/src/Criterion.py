bab8786e415b681166b9dea8fdaa62c04c6c7ea5
Abhinav Goyal <agabohar@gmail.com>
2019-02-20 19:50:12 +0530

    added stability to cross-entropy

--- A3/python/src/Criterion.py
+++ A3/python/src/Criterion.py
@@ -3,10 +3,12 @@ class Criterion():
         return self.forward(input, target)
 
     def forward(self, input, target):
-        return (-input[range(target.size(0)),target.squeeze().long()] + input.exp().sum(dim=1).log()).mean()
+        scaled_logits = input - input.max(dim=1, keepdim=True)[0]
+        return (-scaled_logits[range(target.size(0)),target.squeeze().long()] + scaled_logits.exp().sum(dim=1).log()).mean()
 
     def backward(self, input, target):
-        gradInput = input.softmax(dim=1)
+        scaled_logits = input - input.max(dim=1, keepdim=True)[0]
+        gradInput = (scaled_logits).softmax(dim=1)
         batch_size = target.size(0)
         gradInput[range(batch_size),target.squeeze().long()] -= 1
         gradInput /= batch_size
