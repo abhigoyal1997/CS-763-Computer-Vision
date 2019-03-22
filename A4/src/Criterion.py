cd4e256004f4cf9bd8e267faa3c7d85306dbd219
DIVYANSH PAREEK <divyansh@cse.iitb.ac.in>
2019-03-23 02:05:56 +0530

    Changed Criterion.py for Binary Xent loss. UNTESTED yet.

--- A4/src/Criterion.py
+++ A4/src/Criterion.py
@@ -1,16 +1,20 @@
 # For the special case when it is binary classification (ie, only one logit)
+# Assuming input.shape == (bs,1) and target.shape == (bs,)
 class Criterion():
     def __call__(self, input, target):
         return self.forward(input, target)
 
     def forward(self, input, target):
-        scaled_logits = input - input.max(dim=1, keepdim=True)[0]
-        return (-scaled_logits[range(target.size(0)),target.squeeze().long()] + scaled_logits.exp().sum(dim=1).log()).mean()
+        input_dimmed = input.squeeze().double() # shape == (bs,)
+        p_pred = input_dimmed.sigmoid()
+        p_true = target.squeeze().double()
+        return -(p_true.mul(p_pred.log()) + (1-p_true).mul((1-p_pred).log())).mean()
 
     def backward(self, input, target):
-        scaled_logits = input - input.max(dim=1, keepdim=True)[0]
-        gradInput = (scaled_logits).softmax(dim=1)
+        input_dimmed = input.squeeze().double() # shape == (bs,)
+        p_pred = input_dimmed.sigmoid()
+        p_true = target.squeeze().double()
+        gradInput = (p_pred - p_true)
         batch_size = target.size(0)
-        gradInput[range(batch_size),target.squeeze().long()] -= 1
         gradInput /= batch_size
         return gradInput
