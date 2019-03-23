31c1fbcaf4b099bc01a1e8a99f9578a9c5e1c4fb
DIVYANSH PAREEK <divyansh@cse.iitb.ac.in>
2019-03-23 12:31:22 +0530

    Updated Criterion.py in acc with generic multi-layer vanilla rnn. UNTESTED yet.

--- A4/src/Criterion.py
+++ A4/src/Criterion.py
@@ -1,20 +1,24 @@
-# For the special case when it is binary classification (ie, only one logit)
-# Assuming input.shape == (bs,1) and target.shape == (bs,)
+''' For the special case when it is binary classification (ie, only one logit)
+    Assuming input.shape == (bs,seq_length,1) and target.shape == (bs,)
+'''
+import torch
+
 class Criterion():
     def __call__(self, input, target):
         return self.forward(input, target)
 
     def forward(self, input, target):
-        input_dimmed = input.squeeze().double() # shape == (bs,)
+        input_dimmed = input[:,-1,:].squeeze().double() # shape == (bs,)
         p_pred = input_dimmed.sigmoid()
         p_true = target.squeeze().double()
         return -(p_true.mul(p_pred.log()) + (1-p_true).mul((1-p_pred).log())).mean()
 
     def backward(self, input, target):
-        input_dimmed = input.squeeze().double() # shape == (bs,)
+        input_dimmed = input[:,-1,:].squeeze().double() # shape == (bs,)
         p_pred = input_dimmed.sigmoid()
         p_true = target.squeeze().double()
-        gradInput = (p_pred - p_true)
+        gradInput = torch.zeros_like(input)
+        gradInput[:,-1,0] = (p_pred - p_true)
         batch_size = target.size(0)
         gradInput /= batch_size
         return gradInput
